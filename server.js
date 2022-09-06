2c8b95bcfcbbde17a2e9300ef93363a8a9117260
Matt Porter <matthew.ellis.porter@gmail.com>
2022-09-06 16:25:13 -0400

    mongoose connected

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ require('dotenv').config(); // needed to recognize variables in .env
 
 const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
 // MIDDLEWARE
 
 // Define callback functions for various events
@@ -13,6 +18,8 @@ const db = mongoose.connection
 db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
+
+
 
 // INDEX
 
